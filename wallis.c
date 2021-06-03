1627dcfa65cfc90a723e5f207cc3a5beb95c7c2d
Arun-mec <84711871+Arun-mec@users.noreply.github.com>
2021-06-03 19:34:59 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -5,37 +5,40 @@
 
 float wallis_pi(int);
 
-int main(void) {
+int main(void)
+{
   float pi;
-  for (int i=0; i<5; i++) {
+  for (int i=0; i<5; i++)
+  {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
+    if (!(fabs(pi - M_PI) > 0.15))
+     {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
   }
 
-
-  for (int i=500; i<3000; i++) {
-	  pi = wallis_pi(i);
-	  if (!(fabs(pi - M_PI) < 0.01)) {
-		  printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
-		  abort();
+  for (int i=500; i<3000; i++)
+  {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01))
+    {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
     }
   }
 
-  return (0);
 }
 
-float wallis_pi(int a){
-	int j;
-	float temp,p=1;
-	for(j=1;j<a;j++)
-	{
-		temp=4*j*j;
-		p=p*(temp/(temp-1));
-	}
-	return 2*p;
 
+float wallis_pi(int n)
+{
 
+  float temp,value=1;
+  for(int j=1;j<=n;j++)
+  {
+    temp=4*j*j;
+    value=value*temp/(temp-1);
+  }
+  return value*2;
 }
